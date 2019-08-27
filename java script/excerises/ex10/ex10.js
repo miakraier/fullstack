6a3407abe95d584d80b198f192d2e4e1eb332002
mia <miakraier@gmail.com>
2019-08-27 16:30:29 +0300

    yalla

--- java script/excerises/ex10/ex10.js
+++ java script/excerises/ex10/ex10.js
@@ -1,6 +1,54 @@
-/*להגדיר את כל האלמנטים
-)להגדיר אובייקט עם תכונות ריקות (שם כיתה ציונים
-ו3 פעולות (פונקיות-מינימום מקסימום וממוצע)
-ומחוץ 2 פונקציות  -
-1 שמזינה את הערכים בתוכונות
-2 מגדירה משתנים לכל פעולה (ממוצע מינימום ומקסימום)
+//define all elements in the page
+let name = document.getElementById("name")
+let clas = document.getElementById("class")
+let grades = document.getElementById("grades")
+let Submit = document.getElementById("btn")
+let Calculate = document.getElementById("btn2")
+let result = document.getElementById("result")
+let present = document.getElementById("present")
+let presentt = document.getElementById("student")
+
+//define student object 
+let student = {
+    name: "",
+    clas: "",
+    grades:[],
+    avg(){
+        let sum = 0
+        for(let i =0;i<this.grades.length;i++)
+        sum = sum + parseInt(this.grades[i])
+    return sum/this.grades.length
+},
+min(){
+    let min = 100
+    for(let i =0;i<this.grades.length;i++)
+        if(parseInt(this.grades[i])<min)
+            min=parseInt(this.grades[i])
+    return min
+},
+max(){
+    let max = 0
+    for(let i =0;i<this.grades.length;i++)
+        if(parseInt(this.grades[i])>max)
+            max=parseInt(this.grades[i])
+    return max
+}
+}
+//fuctions
+function studentdet (){
+    student.name=name.value
+    student.clas=clas.value
+    student.grades=grades.value.split(",")
+}
+function results (){
+    let avg = student.avg()
+    let min = student.min()
+    let max = student.max()
+    presentt.innerHTML="hello " + student.name + ","
+    present.innerHTML="this is your yearly data in class " + student.clas + "<br>"
+    present.innerHTML=present.innerHTML + "You minimum grade is "+min + " and you max is "+max +".<br>"
+    present.innerHTML=present.innerHTML + "Your avg is "+avg
+}
+//define event listeners 
+Submit.addEventListener("click", studentdet)
+Calculate.addEventListener("click", results)
